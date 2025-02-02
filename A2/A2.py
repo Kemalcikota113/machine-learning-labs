012948aab871773fccb2fa787e0277e5f6ce2501
Kemalcikota113 <kc222hb@student.lnu.se>
2025-02-02 12:15:24 +0100

    done with first part of A2 (untill page 9)

--- A2/A2.py
+++ A2/A2.py
@@ -22,14 +22,39 @@ print(boston.describe(), end="\n\n")
 
 print("total amount of datapoints: ", boston.shape[0], end="\n\n")
 
-corCoef, pValue = stats.pearsonr(boston['medv'], boston['lstat'])
+# correlation coefficients between our variables for the 3 plots
+corCoef_medv_lstat, pValue_medv_lstat = stats.pearsonr(boston['medv'], boston['lstat'])
+print("Correlation coefficient between medv and lstat: ", corCoef_medv_lstat, ", with p-value: ", pValue_medv_lstat)
 
-print("Correlation coefficient between medv and lstat: ", corCoef, ", with p-value: ", pValue)
+corCoef_medv_lstat, pValue_medv_rm = stats.pearsonr(boston['medv'], boston['rm'])
+print("Correlation coefficient between medv and rm: ", corCoef_medv_lstat, ", with p-value: ", pValue_medv_rm)
 
-sns.lmplot(data=boston, x='lstat', y='medv', hue="black", palette="Blues", height=6, aspect=2)
+corCoef_medv_lstat, pValue_medv_age = stats.pearsonr(boston['medv'], boston['age'])
+print("Correlation coefficient between medv and age: ", corCoef_medv_lstat, ", with p-value: ", pValue_medv_age)
+
+# Scatter plot with regression line between lstat and medv
+sns.regplot(x=boston['lstat'], y=boston['medv'],  line_kws={'color': 'black'})
 
 # Add labels to the plot
 plt.xlabel("percent of households with low socioeconomic status")
+plt.ylabel("median house value")
+plt.title(f"Scatter Plot with Regression Line")
+plt.show() # Remember to make the window bigger to see the plot
+
+# Scatter plot with regression line between rm and medv
+sns.regplot(x=boston['rm'], y=boston['medv'],  line_kws={'color': 'black'})
+
+# Add labels to the plot
+plt.xlabel("average number of rooms per house")
+plt.ylabel("median house value")
+plt.title(f"Scatter Plot with Regression Line")
+plt.show() # Remember to make the window bigger to see the plot
+
+# Scatter plot with regression line between age and medv
+sns.regplot(x=boston['age'], y=boston['medv'],  line_kws={'color': 'black'})
+
+# Add labels to the plot
+plt.xlabel("average age of houses")
 plt.ylabel("median house value")
 plt.title(f"Scatter Plot with Regression Line")
 plt.show() # Remember to make the window bigger to see the plot
